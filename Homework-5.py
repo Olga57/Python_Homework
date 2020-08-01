6e5a01302ff490ba546e429fb6e2fc14d9030bbc
Olga57 <olga_subbotkina@mail.ru>
2020-08-01 08:55:44 +0300

    Задание 2 - ошибка в последней строке - 'tuple' object is not callable - не понимаю, что неправильно.
    Задание 3 - ошибка в последней строке. Также не пойму, в чем ошибка
    
    Задание 4 - ошибок не формуруется, но на рус язык в новом файле не перевел. Почему?

--- Homework-5.py
+++ Homework-5.py
@@ -14,3 +14,31 @@ with open ('text.txt') as f:
         print('{} строка содержит- {} слов'.format(num_line, len(line.split())))
 
 #Задание 3 - фильтр оклад менее 20 тыс. руб.
+with open ('salaries.txt') as f:
+    salaries = []
+    lines = f.readlines()
+    for line in lines:
+        name,salary = line.split('-')
+        salaries.append(int(salary))
+        if int(salary)<20000:
+            print (line, end='')
+    print('\nСредняя зп:', sum(salaries / len(salaries)))
+
+#Задание 4 - перевод на др. язык
+with open ('english.txt', encoding='utf-8') as f:
+    lines=f.readlines()
+
+with open ('russian.txt', 'w', encoding='utf-8') as f:
+    for line in lines:
+        if '1' in lines:
+            line=line.replace('One', 'Один')
+        elif '2' in lines:
+            line=line.replace('Two', 'Два')
+        elif '3' in lines:
+            line = line.replace('Three', 'Три')
+        elif '4' in lines:
+            line=line.replace('Four', 'Четыре')
+        f.write(line)
+
+
+
